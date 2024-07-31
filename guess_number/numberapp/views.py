257bb350888a5e498692aefc3774834856979717
baskarboss00610 <129747388+baskarboss00610@users.noreply.github.com>
2024-07-31 10:31:58 +0530

    Add files via upload

---
+++ guess_number/numberapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+import random
+
+def home(request):
+    # Check for POST request
+    if request.method == 'POST':
+        # Add Secret number to Session
+        secret_number = request.session['secret_number']
+        # Get the guess and convert to integer
+        guess = int(request.POST['guess'])
+        # Increase by 1
+        request.session['attempts'] += 1
+
+        # If the user answers correctly
+        if guess == secret_number:
+            message = 'Congratulations! You guessed the number in {} attempts.'.format(request.session['attempts'])
+            request.session.flush()
+            return render(request, 'result.html', {'message': message})
+        
+        # Else if the guess is greater
+        elif guess < secret_number:
+            message = 'The number is greater than {}!'.format(guess)
+        else:
+            message = 'The number is smaller than {}!'.format(guess)
+
+        # Checking if attempts are used up
+        if request.session['attempts'] >= 5:
+            message = 'Sorry! You have used all 5 attempts. The secret number was {}!.'.format(secret_number)
+            request.session.flush()
+            return render(request, 'result.html', {'message': message})
+
+        return render(request, 'index.html', {'message': message})
+    else:
+        # At the onset this executes
+        # Generate random number from 0 - 999
+        secret_number = random.randint(0, 100)
+        request.session['secret_number'] = secret_number
+        request.session['attempts'] = 0
+        return render(request, 'index.html')
